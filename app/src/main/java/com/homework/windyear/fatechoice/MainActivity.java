5611ae6a68beeb83178b82f4a99de20affed6559
windyear <1280027646@qq.com>
2018-05-27 23:10:11 +0800

    完成了存储主题的功能，还有页面的一些跳转。

--- app/src/main/java/com/homework/windyear/fatechoice/MainActivity.java
+++ app/src/main/java/com/homework/windyear/fatechoice/MainActivity.java
@@ -1,14 +1,17 @@
 package com.homework.windyear.fatechoice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,15 +33,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        Button button = (Button)findViewById(R.id.button);
+        // Button button = (Button)findViewById(R.id.button);
         // 绑定一个时间监听器，跳转到另外一个activity
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, fate_choice.class);
-                startActivity(intent);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, fate_choice.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        // 动态地添加Button和点击事件
+        // 采用 sharedpreferences 进行存储，分别存储主题名字，然后根据主题的名字存储
+        SharedPreferences theme = getSharedPreferences("theme", MODE_PRIVATE);
+        SharedPreferences.Editor editor = theme.edit();
+        // 首先获取主题的名字
+        String theme_name = theme.getString("theme", null);
+        if(theme_name == null){
+            return;
+        }
+        else{
+            String[] all_theme_name = theme_name.split("|");
+            LinearLayout linearLayout = findViewById(R.id.themes);
+            for(int i = 0; i < all_theme_name.length; i++){
+                Button btn = new Button(MainActivity.this);
+                btn.setText(all_theme_name[i]);
+                // 设置按钮居中
+                btn.setGravity(Gravity.CENTER_HORIZONTAL);
+                // 设置事件监听
+                btn.setOnClickListener(click_to_choose);
+                linearLayout.addView(btn);
             }
-        });
+        }
     }
 
     @Override
@@ -62,4 +88,17 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // 一个通用的单击事件，对应每个不同的主题，进入选择activity的时候传入对应主题的名字
+    private View.OnClickListener click_to_choose = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, fate_choice.class);
+            Button btn = (Button)v;
+            // 通过intent传递主题名字到另外一个activity
+            String theme_name = btn.getText().toString();
+            intent.putExtra("theme_name", theme_name);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/com/homework/windyear/fatechoice/add_choice_theme.java
+++ app/src/main/java/com/homework/windyear/fatechoice/add_choice_theme.java
@@ -1,7 +1,13 @@
 package com.homework.windyear.fatechoice;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class add_choice_theme extends AppCompatActivity {
 
@@ -9,5 +15,64 @@ public class add_choice_theme extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_choice_theme);
+        //通过单击按钮添加plate text用于增加选项
+        Button btn_add_choice = findViewById(R.id.button_add_choice);
+        btn_add_choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 获取线性布局
+                LinearLayout linearLayout = findViewById(R.id.linear_choice);
+                // 新建一个plain text
+                EditText editText = new EditText(add_choice_theme.this);
+                editText.setHint("请输入你的神圣选择！");
+                linearLayout.addView(editText);
+            }
+        });
+
+        Button bun_save_choice = findViewById(R.id.button_save_theme);
+        bun_save_choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout linearLayout = findViewById(R.id.linear_choice);
+                String choice = "";
+                for( int i = 0; i < linearLayout.getChildCount(); i++){
+                    View viewChildren = linearLayout.getChildAt(i);
+                    if(viewChildren instanceof  EditText){
+                        EditText editText = (EditText)viewChildren;
+                        // 选项之间使用|分隔开
+                        if(choice != ""){
+                            choice += "|";
+                        }
+                        choice += editText.getText().toString();
+                    }
+                }
+                // 采用 sharedpreferences 进行存储，分别存储主题名字，然后根据主题的名字存储
+                SharedPreferences theme = getSharedPreferences("theme", MODE_PRIVATE);
+                SharedPreferences.Editor editor = theme.edit();
+
+                EditText theme_text = findViewById(R.id.edit_theme);
+                String theme_name = theme_text.getText().toString();
+                if ((theme_name == "") || (choice == "")) {
+                    Toast.makeText(getApplicationContext(), "请填写主题和选项后点击保存主题按钮", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    // 有一个关键字负责存储所有的主题名字, 相同的key会有问题，应该查看是否已经存在相同的主题。
+                    String all_theme_name = theme.getString("theme", null);
+                    if(all_theme_name != ""){
+                        all_theme_name += "|";
+                    }
+                    all_theme_name += theme_name;
+                    editor.putString("theme", all_theme_name);
+                    editor.putString(theme_name, choice);
+                    //之前忘记提交了
+                    editor.commit();
+                    //提交后返回主界面
+                    finish();
+
+                }
+                // String save_choice = theme.getString(theme_name, null);
+                // Toast.makeText(getApplicationContext(), save_choice, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
